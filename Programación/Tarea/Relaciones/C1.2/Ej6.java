2999b296762e7aa674e53dda4889991efe8201f5
LuisMiguel0112 <lgarsev0112@g.educaand.es>
2022-11-03 16:13:53 +0100

    Primer Commit

---
+++ Programación/Tarea/Relaciones/C1.2/Ej6.java
@@ -0,0 +1,15 @@
+
+/*
+ * Luis Miguel García Sevilla 1º DAM Tarde.
+ * Este programa muestra una piramide.
+*/
+
+public class Ej6 {
+    public static void main(String[] args) {
+        System.out.printf("%6s","*\n");
+        System.out.printf("%7s","***\n");
+        System.out.printf("%8s","*****\n");
+        System.out.printf("%9s","*******\n");
+        System.out.printf("%1s","*********\n");
+    }
+}
---
+++ Programación/Tarea/Relaciones/C1.3/Ej7.java
@@ -0,0 +1,15 @@
+
+/*
+ * Luis Miguel García Sevilla 1º DAM Tarde.
+ * Este programa muestra una piramide hueca.
+*/
+
+public class Ej7 {
+    public static void main(String[] args) {
+        System.out.printf("%11s","\033[34m*\n");
+        System.out.printf("%4s %2s","*","*\n");
+        System.out.printf("%3s %4S","*","*\n");
+        System.out.printf("%2s %6s","*","*\n");
+        System.out.printf("%1s","*********\n");
+    }
+}
