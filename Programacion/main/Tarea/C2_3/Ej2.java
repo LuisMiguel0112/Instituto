0f758e5b8c4de1280c0b4819eeafa2f743636de3
LuisMiguel0112 <lgarsev0112@g.educaand.es>
2022-11-08 16:37:41 +0100

    Tarea C2_3

---
+++ Programacion/main/Tarea/C2_3/Ej2.java
@@ -0,0 +1,18 @@
+
+/*
+ * Transformación de euros a pesetas.
+ *
+ * @author Luis Miguel García Sevilla - 1º DAM.
+ * */
+
+package Tarea.C2_3;
+
+public class Ej2 {
+    public static void main(String[] args) {
+        System.out.print("Introduzca la cantidad de euros que quiere convertir: ");
+        String cantidad = System.console().readLine();
+        double euros = Double.parseDouble(cantidad);
+        double pesetas = 166.3860;
+        System.out.println(cantidad+" euros son "+(euros*pesetas)+" pesetas.");
+    }
+}
