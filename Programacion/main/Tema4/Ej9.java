c1c7f0b1c2f5ab815a489b5ca3529974cd9bea53
LuisMiguel0112 <lgarsev0112@g.educaand.es>
2022-11-18 15:33:40 +0100

    correción Ej9 T4

--- Programacion/main/Tema4/Ej9.java
+++ Programacion/main/Tema4/Ej9.java
@@ -22,15 +22,14 @@ public class Ej9 {
     double c = s.nextDouble();
     double x1;
     double x2;
-    double raiz = (Math.sqrt(Math.pow(b,2)-4*a*c));
-    if((a!=0)&&(raiz>=0)&&(raiz<=0)){
-      x1 = (((-b)+raiz)/2*a);
-      x2 = (((-b)-raiz)/2*a);
+    double raiz = (Math.pow(b,2)-4*a*c);
+    if((a!=0)&&(raiz>=0)){
+      x1 = (((-b)+Math.sqrt(raiz))/2*a);
+      x2 = (((-b)-Math.sqrt(raiz))/2*a);
       System.out.println("x1 = "+x1);
       System.out.println("x2 = "+x2);
     }else{
       System.out.println("Esa ecuación no tiene solución real.");
     }
-    System.out.println(raiz);
   }
 }
